a02e21def11cd62e68b0632255fa07ae79a651ef
Davidson Marra <davidson.vieira.ext@vr.com.br>
2022-12-13 16:30:59 -0300

    :sparkles: add create book
    create a handle for create book with params: title and author

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 type Book struct {
-	Id     int
-	Title  string
-	Author string
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
 var Books []Book = []Book{
@@ -22,21 +24,52 @@ func mainHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Bem Vindo!")
 }
 
+func rootBooks(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getAllBooks(w, r)
+	case "POST":
+		createBook(w, r)
+	default:
+		fmt.Fprint(w, "Método não suportado")
+	}
+}
+
 func getAllBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(Books)
 }
 
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("Error reading the body: %v\n", err)
+		return
+	}
+
+	var newBook Book
+	json.Unmarshal(body, &newBook)
+	newBook.Id = len(Books) + 1
+	Books = append(Books, newBook)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(newBook)
+}
+
 func configHandles() {
 	http.HandleFunc("/", mainHandle)
-	http.HandleFunc("/books", getAllBooks)
+	http.HandleFunc("/books", rootBooks)
 }
 
 func configServer() {
 	configHandles()
 
 	fmt.Println("Servidor rodando na porta 1337")
-	http.ListenAndServe(":1337", nil) // DefaultServeMux
+	log.Fatal(http.ListenAndServe(":1337", nil)) // DefaultServeMux
 }
 
 func main() {
