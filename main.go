1974bf7a7bbcbea376a1c8a20b3f9b58251835cd
Davidson Marra <davidson.vieira.ext@vr.com.br>
2022-12-15 00:17:22 -0300

    :sparkles: add gorilla mux
    add a package gorilla/mux to request router

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 type Book struct {
@@ -26,44 +27,17 @@ func mainHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Bem Vindo!")
 }
 
-func rootBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) == 2 || len(parts) == 3 && parts[2] == "" {
-		switch r.Method {
-		case "GET":
-			getAllBooks(w, r)
-		case "POST":
-			createBook(w, r)
-		default:
-			fmt.Fprint(w, "Método não suportado")
-		}
-	} else if len(parts) == 3 || len(parts) == 4 && parts[3] == "" {
-		switch r.Method {
-		case "GET":
-			searchBookById(w, r)
-		case "DELETE":
-			deleteBookById(w, r)
-		case "PUT":
-			updateBookById(w, r)
-		default:
-			fmt.Fprint(w, "Método não suportado")
-		}
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-}
-
 func getAllBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(Books)
 }
 
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Printf("Error reading the body: %v\n", err)
 		return
@@ -79,13 +53,15 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func searchBookById(w http.ResponseWriter, r *http.Request) {
-	parts := strings.Split(r.URL.Path, "/")
+	w.Header().Add("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
 
-	var id, err = strconv.Atoi(parts[2])
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	for _, book := range Books {
 		if book.Id == id {
 			json.NewEncoder(w).Encode(book)
@@ -96,8 +72,14 @@ func searchBookById(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteBookById(w http.ResponseWriter, r *http.Request) {
-	parts := strings.Split(r.URL.Path, "/")
-	id, err := strconv.Atoi(parts[2])
+	w.Header().Add("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -117,15 +99,16 @@ func deleteBookById(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateBookById(w http.ResponseWriter, r *http.Request) {
-	parts := strings.Split(r.URL.Path, "/")
-	id, err := strconv.Atoi(parts[2])
+	w.Header().Add("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	body, errorBody := ioutil.ReadAll(r.Body)
+	body, errorBody := io.ReadAll(r.Body)
 	if errorBody != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -150,19 +133,21 @@ func updateBookById(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 }
 
-func configHandles() {
-	http.HandleFunc("/", mainHandle)
-	http.HandleFunc("/books", rootBooks)
-
-	// e.g. GET /books/{id}
-	http.HandleFunc("/books/", rootBooks)
+func configHandles(router *mux.Router) {
+	router.HandleFunc("/", mainHandle)
+	router.HandleFunc("/books", getAllBooks).Methods("GET")
+	router.HandleFunc("/books", createBook).Methods("POST")
+	router.HandleFunc("/books/{id}", searchBookById).Methods("GET")
+	router.HandleFunc("/books/{id}", updateBookById).Methods("PUT")
+	router.HandleFunc("/books/{id}", deleteBookById).Methods("DELETE")
 }
 
 func configServer() {
-	configHandles()
+	router := mux.NewRouter().StrictSlash(true)
+	configHandles(router)
 
 	fmt.Println("Servidor rodando na porta 1337")
-	log.Fatal(http.ListenAndServe(":1337", nil)) // DefaultServeMux
+	log.Fatal(http.ListenAndServe(":1337", router)) // DefaultServeMux
 }
 
 func main() {
